7c53ecf4ab5213dd8511be6e25e02443bce693f2
Anthony <russo555@gmail.com>
2022-02-02 17:26:13 -0500

    Cleanup.

--- aws/env/us-east/alb.tf
+++ aws/env/us-east/alb.tf
@@ -48,33 +48,3 @@ resource "aws_lb_target_group_attachment" "nomad_clients" {
 output "alb_address" {
     value = "http://${aws_lb.nomad_ingress.dns_name}"
 }
-
-// resource "aws_lb_listener" "consul_listener" {
-//   load_balancer_arn = aws_lb.nomad_ingress.id
-//   port              = 8080
-
-//   default_action {
-//     target_group_arn = aws_lb_target_group.consul.id
-//     type             = "forward"
-//   }
-// }
-
-// resource "aws_lb_target_group" "consul" {
-//   name     = "consul-service"
-//   port     = 8500 
-//   protocol = "HTTP"
-//   vpc_id   = data.aws_vpc.default.id
-
-//   health_check {
-//     port = 8500
-//     path = "/ui"
-//     # Mark healthy if redirected
-//     matcher = "200,301,302"
-//   }
-// }
-
-// resource "aws_lb_target_group_attachment" "consul" {
-//   target_group_arn = aws_lb_target_group.consul.arn
-//   target_id        = aws_instance.server[0].id
-//   port             = 8500
-// }
