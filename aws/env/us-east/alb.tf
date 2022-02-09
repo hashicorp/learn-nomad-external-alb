445819a853a2f87e0b35cd5173fa709eca15fa56
Anthony <russo555@gmail.com>
2022-02-09 13:45:45 -0500

    Updated default forward to balance requests to all nomad clients.

--- aws/env/us-east/alb.tf
+++ aws/env/us-east/alb.tf
@@ -16,7 +16,15 @@ resource "aws_lb_listener" "nomad_listener" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.nomad_clients.arn
+    
+    forward {
+      target_group {
+        arn = aws_lb_target_group.nomad_clients.arn
+      }
+      target_group {
+        arn = aws_lb_target_group.nomad_clients_targeted.arn
+      }
+    }
   }
 }
 
